719fee6ffd14a7f960f90d87263d34aaa4c70c84
Andrés Flores <andresfmcs97@gmail.com>
2018-01-04 22:00:47 -0600

    disparador historico actualizado

--- Disparadores/trigger_historico.sql
+++ Disparadores/trigger_historico.sql
@@ -1,24 +1,21 @@
 --creamos una secuencia para la llave de cada tupla del histórico
 create sequence hist_seq start with 1;
 
---creamos un trigger que se encargará de colocar el respectivo id a la nueva tupla del histórico
-create or replace trigger dept_bir 
-before insert on historico 
-for each row
---colocamos el nuevo valor de la secuencia como id del nuevo renglón del historico
-begin
-  select hist_seq.nextval
-  into   :new.idhistorico
-  from   dual;
-end;
-
+delete trigger dept_bir;
 --creamos disparador para almacenar la informacion necesaria en el historico
 create or replace trigger actualiza_historico
 after insert or update on producto
 for each row
+declare
+	new_idHistorico;
 begin
-	insert into historico(idProducto, fechaActualizacion, precioNuevo)
-	               values(:new.idProducto, current_date,:new.precio);
+    select hist_seq.nextval
+    into new_idHistorico
+    from dual; 
+	insert into historico(idHistorico, idProducto, fechaActualizacion, precioNuevo)
+	               values(new_idHistorico, :new.idProducto, current_date,:new.precio);
+	insert into conservar(idHistorico, idProducto) 
+		           values(new_idHistorico, :new.idProducto);
 end;
 
 /*
