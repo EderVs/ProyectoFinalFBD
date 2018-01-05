f72811d05d0a1914799d3f37a1576b735d58e891
Andrés Flores <andresfmcs97@gmail.com>
2018-01-04 22:07:02 -0600

    disparador historico actualizado

--- Disparadores/trigger_historico.sql
+++ Disparadores/trigger_historico.sql
@@ -1,7 +1,6 @@
 --creamos una secuencia para la llave de cada tupla del histórico
 create sequence hist_seq start with 1;
-
-delete trigger dept_bir;
+	
 --creamos disparador para almacenar la informacion necesaria en el historico
 create or replace trigger actualiza_historico
 after insert or update on producto
