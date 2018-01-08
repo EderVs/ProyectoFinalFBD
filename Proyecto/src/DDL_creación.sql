cb765a2c844e78caff97d5cf469692a6ba2aaef4
MiguelConcha <mconcha@ciencias.unam.mx>
2018-01-08 14:26:15 -0600

    Revisión y ligeros cambios en restricciones.

---
+++ Proyecto/src/DDL_creación.sql
@@ -0,0 +1,242 @@
+##########################################################################
+-- Nombre            : DDL_creación.sql.
+-- Fecha             : 12 de enero del 2018.
+-- Autores           : Flores Martínez Andrés, 
+--                     Vázquez Salcedo Eduardo Eder,
+--                     Sánchez Pérez Pedro Juan Salvador,
+--                     Concha Vázquez Miguel.
+-- Compañía          : Computólogos A.C., Facultad de Ciencias UNAM.
+-- Cliente           : Taquería Tacoste.
+-- ========================================================================
+-- Propósito         : A través de las sentencias del lenguje de definición 
+--                     de datos (DDL, "Data Definition Language") del SMBD
+--                     Oracle, se efectúa la construcción de la base de 
+--                     datos de acuerdo con el esquema lógico obtenido 
+--                     luego de su normalización a BCNF que siguió de la 
+--                     previa abstracción del sistema luego del modelo
+--                     conceptual. Se crean a continuación las relaciones
+--                     (tablas) y se especifican los distintos atributos con 
+--                     sus respectivos dominios.  
+--                     Se resuelve la integridad de dominios de marca nulos
+--                     al especificar que no son aceptador al ser sujetos
+--                     a potenciales ambigüedades.
+##########################################################################
+CREATE TABLE Cliente(
+        taquiClave INTEGER NOT NULL,
+        email VARCHAR2(50) NOT NULL,
+        telefono VARCHAR2(20) NOT NULL,
+        nombre VARCHAR2(50) NOT NULL,
+        apellidoPaterno VARCHAR2(50) NOT NULL,
+        apellidoMaterno VARCHAR2(50) NOT NULL,
+        municipio VARCHAR2(100) NOT NULL,
+        colonia VARCHAR2(100) NOT NULL,
+        calle VARCHAR2(100) NOT NULL,
+        CP NUMBER(5) NOT NULL,
+        numInterior INTEGER NOT NULL,
+        numExterior INTEGER NOT NULL,
+        fechaPrimerVista DATE NOT NULL,
+        numPuntos INTEGER NOT NULL
+);
+
+CREATE TABLE Conservar(
+        idProducto INTEGER NOT NULL,
+        idHistorico INTEGER NOT NULL
+);
+
+CREATE TABLE Contener(
+        numPedido INTEGER NOT NULL,
+        idProducto INTEGER NOT NULL,
+        cantidad INTEGER NOT NULL
+);
+
+CREATE TABLE CPEdoCliente(
+        CP NUMBER(5) NOT NULL,
+        estado VARCHAR2(100) NOT NULL
+);
+
+CREATE TABLE CPEdoEmpleado(
+        CP NUMBER(5) NOT NULL,
+        estado VARCHAR2(100) NOT NULL
+);
+
+CREATE TABLE CPEdoProveedor(
+        CP NUMBER(5) NOT NULL,
+        estado varchar2(100) NOT NULL
+);
+
+CREATE TABLE CPEdoSucursal(
+        CP NUMBER(5) NOT NULL,
+        estado VARCHAR2(100) NOT NULL
+);
+
+CREATE TABLE CURPFnacEmp(
+        CURP CHAR(18) NOT NULL,
+        fechaNac DATE NOT NULL
+);
+
+CREATE TABLE FechaPedPromo(
+        fechaPedido DATE NOT NULL,
+        promocion VARCHAR2(50) NOT NULL
+);
+
+CREATE TABLE Dirigir(
+        idSucursal INTEGER NOT NULL,
+        taquiClave INTEGER NOT NULL,
+        fechaInicio DATE NOT NULL
+);
+
+CREATE TABLE Empleado(
+        taquiClave INTEGER NOT NULL,
+        idSucursal INTEGER NOT NULL,
+        salario FLOAT(2) NOT NULL,
+        email VARCHAR2(50) NOT NULL,
+        telefono VARCHAR2(20) NOT NULL,
+        nombre VARCHAR2(50) NOT NULL,
+        apellidoPaterno VARCHAR2(50) NOT NULL,
+        apellidoMaterno VARCHAR2(50) NOT NULL,
+        municipio VARCHAR2(100) NOT NULL,
+        colonia VARCHAR2(100) NOT NULL,
+        calle VARCHAR2(100) NOT NULL,
+        CP NUMBER(5) NOT NULL,
+        numInterior INTEGER NOT NULL,
+        numExterior INTEGER NOT NULL,
+        CURP CHAR(18) NOT NULL,
+        RFC CHAR(13) NOT NULL,
+        tipo VARCHAR2(40) NOT NULL,
+        tipoSangre varchar2(5) NOT NULL,
+        numEmergencia VARCHAR2(20) NOT NULL,
+        fechaContratacion DATE NOT NULL
+);
+
+CREATE TABLE Historico(
+        idHistorico INTEGER NOT NULL,
+        idProducto INTEGER NOT NULL,
+        fechaActualizacion DATE NOT NULL,
+        precioPrevio FLOAT(2) NOT NULL,
+        precioNuevo FLOAT(2) NOT NULL
+);
+
+CREATE TABLE Ingrediente(
+        idIngrediente INTEGER NOT NULL,
+        nombre VARCHAR2(50) NOT NULL,
+        marca VARCHAR2(60) NOT NULL,
+        cantidadExistencia FLOAT(3) NOT NULL,
+        fechaCaducidad DATE NOT NULL
+);
+
+CREATE TABLE Licencia(
+        taquiClave INTEGER NOT NULL,
+        codigo VARCHAR2(30) NOT NULL
+);
+
+CREATE TABLE Llevar(
+        taquiClave INTEGER NOT NULL,
+        numPedido INTEGER NOT NULL
+);
+
+CREATE TABLE Mobiliario(
+        idMueble INTEGER NOT NULL,
+        tipo VARCHAR2(30) NOT NULL
+);
+
+CREATE TABLE Pedido(
+        numPedido INTEGER NOT NULL,
+        idSucursal INTEGER NOT NULL,
+        fechaPedido DATE NOT NULL,
+        taquiClave INTEGER NOT NULL,
+        metodoPago VARCHAR2(50) NOT NULL
+);
+
+CREATE TABLE Poseer(
+        taquiClave INTEGER NOT NULL,
+        idTransporte INTEGER NOT NULL
+);
+
+CREATE TABLE Producto(
+        idProducto INTEGER NOT NULL,
+        puntosOtorgar INTEGER NOT NULL,
+        nombre varchar2(70) NOT NULL,
+        precio FLOAT(2) NOT NULL,
+        taquegoria VARCHAR2(50) NOT NULL
+);
+
+CREATE TABLE ProductoLeyenda(
+        idProducto INTEGER NOT NULL,
+        leyenda VARCHAR2(50) NOT NULL
+);
+
+CREATE TABLE Proveedor(
+        RFC CHAR(13) NOT NULL,
+        razonSocial VARCHAR2(100) NOT NULL,
+        inicioRelacion DATE NOT NULL,
+        email VARCHAR2(50) NOT NULL,
+        telefono VARCHAR2(20) NOT NULL,
+        municipio VARCHAR2(100) NOT NULL,
+        colonia VARCHAR2(100) NOT NULL,
+        calle VARCHAR2(100) NOT NULL,
+        CP NUMBER(5) NOT NULL,
+        numInterior INTEGER NOT NULL, 
+        numExterior INTEGER NOT NULL
+);
+
+CREATE TABLE ProveerIng(
+        RFC CHAR(13) NOT NULL,
+        idIngrediente INTEGER NOT NULL,
+        precio FLOAT(2) NOT NULL
+);
+
+CREATE TABLE ProveerMob(
+        RFC CHAR (13) NOT NULL,
+        idMueble INTEGER NOT NULL,
+        precio FLOAT(2) NOT NULL
+);
+CREATE TABLE Recomendar(
+        idProducto INTEGER NOT NULL,
+        idProductoSalsa INTEGER NOT NULL
+);
+
+CREATE TABLE Salsa(
+        idProducto INTEGER NOT NULL,
+        presentacion VARCHAR2(70) NOT NULL,
+        scoville VARCHAR2(20) NOT NULL
+);
+
+CREATE TABLE Sucursal(
+        idSucursal INTEGER NOT NULL,
+        horaApertura TIMESTAMP NOT NULL,
+        horaCierre TIMESTAMP NOT NULL,
+        municipio VARCHAR2(100) NOT NULL,
+        colonia VARCHAR2(100) NOT NULL,
+        calle VARCHAR2(100) NOT NULL,
+        CP NUMBER(5) NOT NULL,
+        numInterior INTEGER NOT NULL,
+        numExterior INTEGER NOT NULL
+);
+
+CREATE TABLE SucursalTelefono(
+        idSucursal INTEGER NOT NULL,
+        telefono VARCHAR2(20) NOT NULL
+);
+
+CREATE TABLE Supervisar(
+        taquiClaveGerente INTEGER NOT NULL,
+        taquiClaveSupervisado INTEGER NOT NULL
+);
+
+CREATE TABLE TacoRider(
+        taquiClave INTEGER NOT NULL,
+        estaDisponible NUMBER(1) NOT NULL
+);
+
+CREATE TABLE Tener(
+        idProducto INTEGER NOT NULL,
+        idIngrediente INTEGER NOT NULL,
+        cantidad FLOAT(3) NOT NULL
+);
+
+CREATE TABLE Transporte(
+        idTransporte INTEGER NOT NULL,
+        tipo VARCHAR2(50) NOT NULL,
+        marca VARCHAR2(55) NOT NULL,
+        modelo VARCHAR2(70) NOT NULL
+);
---
+++ Proyecto/src/DDL_restricciones.sql
@@ -0,0 +1,122 @@
+##########################################################################
+-- Nombre            : DDL_restricciones.sql.
+-- Fecha             : 12 de enero del 2018.
+-- Autores           : Flores Martínez Andrés, 
+--                     Vázquez Salcedo Eduardo Eder,
+--                     Sánchez Pérez Pedro Juan Salvador,
+--                     Concha Vázquez Miguel.
+-- Compañía          : Computólogos A.C., Facultad de Ciencias UNAM.
+-- Cliente           : Taquería Tacoste.
+-- ========================================================================
+-- Propósito         : Con las sentencias de alteración de tablas del DDL
+--                     se especifican las respectivas restricciones
+--                     ("constraints") de las relaciones, correspondientes
+--                     a las de entidad de llaves primarias, de entidad de 
+--                     llaves foráneas y las de dominio (chequeos y unicidad).
+--                     Lo corresondiente a las marcas NULL ya fue manejado
+--                     directamente en el archivo DDL_creación.sql  
+##########################################################################
+
+/**
+  * INTEGRIDADES DE ENTIDAD DE LLAVES PRIMARIAS.
+  * Estableciendo las llaves primarias simples y compuestas de las tablas.
+ */
+ALTER TABLE Cliente ADD CONSTRAINT pk_taquiClave_c PRIMARY KEY(taquiClave);
+ALTER TABLE Conservar ADD CONSTRAINT pk_ProdHist PRIMARY KEY(idProducto,idHistorico);
+ALTER TABLE Contener ADD CONSTRAINT pk_numPedidProd PRIMARY KEY(numPedido,idProducto);
+ALTER TABLE CPEdoCliente ADD CONSTRAINT pk_cp_edoc PRIMARY KEY(CP);
+ALTER TABLE CPEdoEmpleado ADD CONSTRAINT pk_cp_edoemp PRIMARY KEY(CP);
+ALTER TABLE CPEdoProveedor ADD CONSTRAINT pk_cp_edoprov PRIMARY KEY(CP);
+ALTER TABLE CPEdoSucursal ADD CONSTRAINT pk_cp_edos PRIMARY KEY(CP);
+ALTER TABLE CURPFnacEmp ADD CONSTRAINT pk_curp PRIMARY KEY(CURP);
+ALTER TABLE FechaPedPromo ADD CONSTRAINT pk_fechaPedProm PRIMARY KEY(fechaPedido);
+ALTER TABLE Dirigir ADD CONSTRAINT pk_idSucursal_d PRIMARY KEY(idSucursal,taquiClave);
+ALTER TABLE Empleado ADD CONSTRAINT pk_taquiClave_e PRIMARY KEY(taquiClave);
+ALTER TABLE Historico ADD CONSTRAINT pk_idHistorico PRIMARY KEY(idHistorico);
+ALTER TABLE Ingrediente ADD CONSTRAINT pk_idIngrediente PRIMARY KEY(idIngrediente);
+ALTER TABLE Licencia ADD CONSTRAINT pk_taquiClave_l PRIMARY KEY(taquiClave);
+ALTER TABLE Llevar ADD CONSTRAINT pk_clavePed PRIMARY KEY(taquiClave,numPedido);
+ALTER TABLE Mobiliario ADD CONSTRAINT pk_idMueble PRIMARY KEY(idMueble);
+ALTER TABLE Pedido ADD CONSTRAINT pk_numPedido PRIMARY KEY(numPedido);
+ALTER TABLE Poseer ADD CONSTRAINT pk_taquiClave_po PRIMARY KEY(taquiClave,idTransporte);
+ALTER TABLE Producto ADD CONSTRAINT pk_idProducto_p PRIMARY KEY(idProducto);
+ALTER TABLE ProductoLeyenda ADD CONSTRAINT pk_idProducto_pl PRIMARY KEY(idProducto,leyenda);
+ALTER TABLE Proveedor ADD CONSTRAINT pk_rfc PRIMARY KEY(RFC);
+ALTER TABLE ProveerIng ADD CONSTRAINT pk_ProvIng PRIMARY KEY(RFC,idIngrediente);
+ALTER TABLE ProveerMob ADD CONSTRAINT pk_ProvMob PRIMARY KEY(RFC,idMueble);
+ALTER TABLE Recomendar ADD CONSTRAINT pk_GerSub_r PRIMARY KEY(idProducto,idProductoSalsa);
+ALTER TABLE Salsa ADD CONSTRAINT pk_idProducto_s PRIMARY KEY(idProducto);
+ALTER TABLE Sucursal ADD CONSTRAINT pk_idSucursal_s PRIMARY KEY(idSucursal);
+ALTER TABLE SucursalTelefono ADD CONSTRAINT pk_tel_idSucursal PRIMARY KEY(telefono);
+ALTER TABLE Supervisar ADD CONSTRAINT pk_GerSub_sup PRIMARY KEY(taquiClaveGerente,taquiClaveSupervisado);
+ALTER TABLE TacoRider ADD CONSTRAINT pk_taquiClave_tr PRIMARY KEY(taquiClave);
+ALTER TABLE Tener ADD CONSTRAINT pk_idProdidIng PRIMARY KEY(idProducto,idIngrediente);
+ALTER TABLE Transporte ADD CONSTRAINT pk_idTransporte PRIMARY KEY(idTransporte);
+
+/**
+  * INTEGRIDADES DE ENTIDAD DE LLAVES FORÉNEAS.
+  * Estableciendo las restricciones de llaves externas para lograr la conexión entre las tablas.
+  * Estamos asegurando la integridad referencial al usar casada porque al eliminar un registro de una tabla
+  * referenciada, se borran también en las tablas rerenciantes.
+ */
+ALTER TABLE Conservar ADD CONSTRAINT fk_idProducto_cons FOREIGN KEY(idProducto) REFERENCES Producto(idProducto) ON DELETE CASCADE;
+ALTER TABLE Conservar ADD CONSTRAINT fk_idHistorico FOREIGN KEY(idHistorico) REFERENCES Historico(idHistorico) ON DELETE CASCADE;
+ALTER TABLE Contener ADD CONSTRAINT fk_idnumPedido FOREIGN KEY(numPedido) REFERENCES Pedido(numPedido) ON DELETE CASCADE;
+ALTER TABLE Contener ADD CONSTRAINT fk_idProducto_cont FOREIGN KEY(idProducto) REFERENCES Producto(idProducto) ON DELETE CASCADE;
+ALTER TABLE Dirigir ADD CONSTRAINT fk_idSucursal_d FOREIGN KEY(idSucursal) REFERENCES Sucursal(idSucursal) ON DELETE CASCADE;
+ALTER TABLE Dirigir ADD CONSTRAINT fk_taquiClave_d FOREIGN KEY(taquiClave) REFERENCES Empleado(taquiClave) ON DELETE CASCADE;
+ALTER TABLE Empleado ADD CONSTRAINT fk_empleado FOREIGN KEY(idSucursal) REFERENCES Sucursal(idSucursal) ON DELETE CASCADE;
+ALTER TABLE Historico ADD CONSTRAINT fk_idProducto_h FOREIGN KEY(idProducto) REFERENCES Producto(idProducto) ON DELETE CASCADE;
+ALTER TABLE Licencia ADD CONSTRAINT fk_taquiClave_li FOREIGN KEY(taquiClave) REFERENCES TacoRider(taquiClave) ON DELETE CASCADE;
+ALTER TABLE Llevar ADD CONSTRAINT fk_numPedido FOREIGN KEY(numPedido) REFERENCES Pedido(numPedido) ON DELETE CASCADE;
+ALTER TABLE Llevar ADD CONSTRAINT fk_taquiClave_ll FOREIGN KEY(taquiClave) REFERENCES TacoRider(taquiClave) ON DELETE CASCADE;
+ALTER TABLE Pedido ADD CONSTRAINT fk_idSucursal_p FOREIGN KEY(idSucursal) REFERENCES Sucursal(idSucursal) ON DELETE CASCADE;
+ALTER TABLE Pedido ADD CONSTRAINT fk_taquiClave_p FOREIGN KEY(taquiClave) REFERENCES Cliente(taquiClave) ON DELETE CASCADE;
+ALTER TABLE Poseer ADD CONSTRAINT fk_taquiClave_pos FOREIGN KEY(taquiClave) REFERENCES TacoRider(taquiClave) ON DELETE CASCADE;
+ALTER TABLE Poseer ADD CONSTRAINT fk_idTransporte FOREIGN KEY(idTransporte) REFERENCES Transporte(idTransporte) ON DELETE CASCADE;
+ALTER TABLE ProductoLeyenda ADD CONSTRAINT fk_idProducto_pl FOREIGN KEY(idProducto) REFERENCES Producto(idProducto) ON DELETE CASCADE;
+ALTER TABLE ProveerIng ADD CONSTRAINT fk_rfc_ping FOREIGN KEY(RFC) REFERENCES Proveedor(RFC) ON DELETE CASCADE;
+ALTER TABLE ProveerIng ADD CONSTRAINT fk_idIngrediente_pr FOREIGN KEY(idIngrediente) REFERENCES Ingrediente(idIngrediente) ON DELETE CASCADE;
+ALTER TABLE ProveerMob ADD CONSTRAINT fk_rfc_pmob FOREIGN KEY(RFC) REFERENCES Proveedor(RFC) ON DELETE CASCADE;
+ALTER TABLE ProveerMob ADD CONSTRAINT fk_idMueble FOREIGN KEY(idMueble) REFERENCES Mobiliario(idMueble) ON DELETE CASCADE;
+ALTER TABLE Recomendar ADD CONSTRAINT fk_idProducto_rec FOREIGN KEY(idProducto) REFERENCES Producto(idProducto) ON DELETE CASCADE;
+ALTER TABLE Salsa ADD CONSTRAINT fk_idProducto_s FOREIGN KEY(idProducto) REFERENCES Producto(idProducto) ON DELETE CASCADE;
+ALTER TABLE Recomendar ADD CONSTRAINT fk_idProductoSalsa FOREIGN KEY(idProductoSalsa) REFERENCES Salsa(idProducto) ON DELETE CASCADE;
+ALTER TABLE SucursalTelefono ADD CONSTRAINT fk_idSucursal_st FOREIGN KEY(idSucursal) REFERENCES Sucursal(idSucursal) ON DELETE CASCADE;
+ALTER TABLE Supervisar ADD CONSTRAINT fk_tcGer FOREIGN KEY(taquiClaveGerente) REFERENCES Empleado(taquiClave) ON DELETE CASCADE;
+ALTER TABLE Supervisar ADD CONSTRAINT fk_tcSub FOREIGN KEY(taquiClaveSupervisado) REFERENCES Empleado(taquiClave) ON DELETE CASCADE;
+ALTER TABLE TacoRider ADD CONSTRAINT fk_taquiClave_tr FOREIGN KEY(taquiClave) REFERENCES Empleado(taquiClave) ON DELETE CASCADE;
+ALTER TABLE Tener ADD CONSTRAINT fk_idProducto_ten FOREIGN KEY(idProducto) REFERENCES Producto(idProducto) ON DELETE CASCADE;
+ALTER TABLE Tener ADD CONSTRAINT fk_idIngrediente_ten FOREIGN KEY(idIngrediente) REFERENCES Ingrediente(idIngrediente) ON DELETE CASCADE;
+
+/**
+  * INTEGRIDADES DE DOMINIO: UNICIDAD y CHECKS. No incluimos en la base de datos valores nulos que pueden 
+  * prestarse a diversas interpretaciones como son datos que no aplican, valores desconocidos, entre otros.
+ */
+ -- Checks:
+ALTER TABLE Cliente ADD CONSTRAINT ch_num_puntos_cl CHECK (numPuntos >= 0); -- No se puede tener una cantidad negativa de puntos.
+ALTER TABLE Contener ADD CONSTRAINT ch_cantidad_cont CHECK (cantidad >= 0); --No puede tener un pedido una cantidad negativa de productos.
+ALTER TABLE Empleado ADD CONSTRAINT ch_tipoSangre CHECK (tipoSangre IN ('O+','O-','A+','A-','B+','B-','AB+','AB-'));
+ALTER TABLE Empleado ADD CONSTRAINT ch_tipo_emp CHECK (tipo IN ('PARRILLERO','TAQUERO','MESERO','CAJERO','TORTILLERO','TACORIDER'));
+ALTER TABLE Empleado ADD CONSTRAINT ch_salario CHECK (salario >= 0); --Quizá trabajen sin sueldo por un periodo de prueba.
+ALTER TABLE FechaPedPromo ADD CONSTRAINT ch_promo CHECK (promocion IN ('JUEVES POZOLERO','TACO AMIGO', 'MARTES DE TORTUGA', 'NINGUNA'));
+ALTER TABLE Historico ADD CONSTRAINT ch_precio_prevh CHECK (precioPrevio >= 0); --Quizás haya cosas gratis a veces, pero no puede ser negativo.
+ALTER TABLE Historico ADD CONSTRAINT ch_precio_nuevh CHECK (precioNuevo >= 0); --Quizás haya cosas gratis a veces, pero no puede ser negativo.
+ALTER TABLE Ingrediente ADD CONSTRAINT ch_cantidadExistencia CHECK (cantidadExistencia >= 0); --No puede faltar negativamente.
+ALTER TABLE Pedido ADD CONSTRAINT ch_metodoPago CHECK (metodoPago IN ('EFECTIVO','TARJETA DEBITO','TARJETA CREDITO','CRYPTOCURRENCY','VALES'));
+ALTER TABLE Producto ADD CONSTRAINT ch_num_puntos_prod CHECK (puntosOtorgar >= 0); --No podemos asignar puntos negativos.
+ALTER TABLE Producto ADD CONSTRAINT ch_precio_prod CHECK (precio >= 0); --Quizás haya cosas gratis a veces.
+ALTER TABLE Producto ADD CONSTRAINT ch_taquegoria CHECK (taquegoria IN('ENTRADAS','DEL CAZO', 'SOPES', 'HUARACHES','GRINGAS','ENCHILADAS','QUESOS','QUECAS','VOLCANES','ENSALADAS','TACOS','HAMBURGUESAS','TORTAS','BEBIDAS','POSTRES')); --Quizás haya cosas gratis a veces.
+ALTER TABLE ProductoLeyenda ADD CONSTRAINT ch_ley CHECK (leyenda IN ('VEGANO','ESPECIAL','ORGANICO','DELUXE','RECOMENDACION','LIGHT','HOT')); --No puede tener un producto una cantidad negativa de un ingrediente.
+ALTER TABLE ProveerIng ADD CONSTRAINT ch_precio_ping CHECK (precio >= 0); --Tal vez como parte de una oferta le dé cosas gratis al local.
+ALTER TABLE ProveerMob ADD CONSTRAINT ch_precio_pmob CHECK (precio >= 0); --Tal vez como parte de una oferta le dé cosas gratis al local.
+ALTER TABLE Salsa ADD CONSTRAINT ch_scoville  CHECK (scoville IN ('DULCE','BAJO','MEDIO','ALTO','EXTREMO'));
+ALTER TABLE Salsa ADD CONSTRAINT ch_psalsa CHECK (presentacion IN ('300ml', '500ml', '1L', '2L')); --No puede tener un producto una cantidad negativa de un ingrediente.
+ALTER TABLE Sucursal ADD CONSTRAINT ch_horario CHECK (horaApertura < horaCierre);
+ALTER TABLE Tener ADD CONSTRAINT ch_cantidad_ten CHECK (cantidad >= 0); --No puede tener un producto una cantidad negativa de un ingrediente.
+ALTER TABLE Transporte ADD CONSTRAINT ch_tipo_tr  CHECK (tipo IN ('BICICLETA','MOTOCICLETA'));
+-- Unicidad:
+ALTER TABLE Cliente ADD CONSTRAINT unq_email_cl UNIQUE (email); --Hay una dirección de correo electrónico para cada cliente; no coinciden.
+ALTER TABLE Empleado ADD CONSTRAINT unq_email_emp UNIQUE (email); --Hay una dirección de correo electrónico para cada empleado; no coinciden.
+ALTER TABLE Proveedor ADD CONSTRAINT unq_razonSocial_prov UNIQUE (razonSocial); --No puede haber legalmente dos proveedores con la misma razón social. 
+ALTER TABLE Proveedor ADD CONSTRAINT unq_email_prov UNIQUE (email); --Hay una direccón de correo electrónico para cada proveedor; no coinciden.
