563da034fef04948f38ffd5a966286ace9b918cb
MiguelConcha <mconcha@ciencias.unam.mx>
2018-01-10 13:31:28 -0600

    Comentarios en ingredientes trigger.

--- Proyecto/src/Disparadores/trigger_ingredientes.sql
+++ Proyecto/src/Disparadores/trigger_ingredientes.sql
@@ -1,15 +1,29 @@
---deseamos que cada que se haga un producto (lo cual sabemos cuando se agrega un elemento a contener)
---actualicemos la cantidad en existencia de dicho ingrediente, además verificaremos si es posible
---realizar todo el pedido con la cantidad de ingredientes en almacén
+-- ##########################################################################
+-- Nombre            : trigger_histórico.sql.
+-- Fecha             : 12 de enero del 2018.
+-- Autores           : Flores Martínez Andrés, 
+--                     Vázquez Salcedo Eduardo Eder,
+--                     Sánchez Pérez Pedro Juan Salvador,
+--                     Concha Vázquez Miguel.
+-- Compañía          : Computólogos A.C., Facultad de Ciencias UNAM.
+-- Cliente           : Taquería Tacoste.
+-- ========================================================================
+-- Propósito         : Deseamos que cada que se haga un producto (lo cual sabemos 
+--                     cuando se agrega un elemento a contener) actualicemos la 
+--                     cantidad en existencia de dicho ingrediente, además
+--                     verificaremos si es posible realizar todo el pedido con
+--                     la cantidad de ingredientes en almacén.
+-- ##########################################################################
+
 create or replace trigger update_ingredientes
---en la tabla contener agregamos los productos y su respectiva cantidad de cada pedido
---por lo cual cada que agreguemos un producto restamos la cantidad de ingredientes utilizadas para dicho producto
+-- En la tabla contener agregamos los productos y su respectiva cantidad de cada pedido,
+-- por lo cual cada que agreguemos un producto restamos la cantidad de ingredientes utilizadas para dicho producto.
 after insert or update on contener
 for each row
 declare
 	c float(3);
 	possible number(1);
-	--agrupamos por ingrediente la cantidad necesaria para realizar todo el pedido
+	-- Agrupamos por ingrediente la cantidad necesaria para realizar todo el pedido.
 	cursor my_query is select idIngrediente, sum(:new.cantidad*t.cantidad) as cant
                        from producto p, tener t
                        where :new.idProducto = p.idProducto and p.idProducto = t.idProducto
@@ -17,13 +31,13 @@ declare
     esc_ing varchar2(50);
 begin
 	possible := 1;
-    --verificamos si la cantidad de cada ingrediente alcanza para realizar todo el pedido
+    -- Verificamos si la cantidad de cada ingrediente alcanza para realizar todo el pedido.
 	for ingcan in my_query
 	LOOP
-		--almacenamos en la variable la cantidad en existencia del ingrediente actual en la iteración
+		-- Almacenamos en la variable la cantidad en existencia del ingrediente actual en la iteración.
 		select cantidadExistencia into c
 		from (select cantidadExistencia from ingrediente where ingcan.idIngrediente = idIngrediente);
-		--si se necesita más ingrediente del disponible para algún ingrediente, reportamos un error
+		-- Si se necesita más ingrediente del disponible para algún ingrediente, reportamos un error.
 		if ingcan.cant > c then
 			select nombre into esc_ing 
 			from (select nombre from ingrediente where ingcan.idIngrediente = idIngrediente);
@@ -31,10 +45,10 @@ begin
 			exit;
 		end if;
 	end LOOP;
-    --reportamos error
+    -- Reportamos error:
 	if possible = 0 then 
 		raise_application_error(-20003,'La cantidad en existencia del ingrediente '||esc_ing||' no es suficiente para realizar el pedido.');
-	--si hay suficientes ingredientes para todos los productos entonces los utilizamos y actualizamos la cantidad en existencia
+	-- Si hay suficientes ingredientes para todos los productos entonces los utilizamos y actualizamos la cantidad en existencia.
 	else
 		for ingcan in my_query
 		LOOP
@@ -45,6 +59,7 @@ begin
 	end if;	
 end;
 
+-- Ejemplos:
 /*
 INSERT INTO SUCURSAL(IDSUCURSAL, HORAAPERTURA, HORACIERRE, MUNICIPIO, COLONIA, CALLE, CP, NUMINTERIOR, NUMEXTERIOR) VALUES
 (1, TO_TIMESTAMP ('10-Sep-02 11:10:10.123000', 'DD-Mon-RR HH24:MI:SS.FF'),TO_TIMESTAMP ('10-Sep-02 14:10:10.123000', 'DD-Mon-RR HH24:MI:SS.FF'), 'Elkland', 'Fakenham', '1954 Monument Road', 0, 95, 3914);
