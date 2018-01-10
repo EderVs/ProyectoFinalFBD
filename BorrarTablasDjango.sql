430103ba276efc299fb4980f6b9898a0c1b2c46a
edervs <eedervs.97@gmail.com>
2018-01-10 17:55:43 -0600

    Modificando BorrarTablasDjango

--- BorrarTablasDjango.sql
+++ BorrarTablasDjango.sql
@@ -2,7 +2,7 @@ drop table "AUTH_GROUP" cascade constraints;
 drop table "AUTH_GROUP_PERMISSIONS" cascade constraints;
 drop table "AUTH_PERMISSION" cascade constraints;
 drop table "AUTH_USER" cascade constraints;
-drop table "AUTa_USER_GROUPS" cascade constraints;
+drop table "AUTH_USER_GROUPS" cascade constraints;
 drop table "AUTH_USER_USER_PERMISSIONS" cascade constraints;
 drop table "DJANGO_ADMIN_LOG" cascade constraints;
 drop table "DJANGO_CONTENT_TYPE" cascade constraints;
