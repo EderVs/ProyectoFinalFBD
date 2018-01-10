5aa72ea4dee5e38ea93edb70c6052a5cbe18dae6
MiguelConcha <mconcha@ciencias.unam.mx>
2018-01-10 13:21:35 -0600

    Cambios en el trigger.

--- Proyecto/src/Disparadores/trigger_taquegoria.sql
+++ Proyecto/src/Disparadores/trigger_taquegoria.sql
@@ -7,33 +7,33 @@ declare
 begin  
         nom := :new.nombre;
         if nom in ('Nopal asado','Frijoles refritos','Frijoles refritos con chorizo','Don totopo','Don totopo especial','Cebollitas al carbon','Papas a la francesa','Guacamole','Chicharron de queso','Los sopecitos','Entrada') then
-            insert into categoria(idProducto,taqueGoria) values(:new.idProducto, 'Entradas');
+            insert into categoria(idProducto,taqueGoria) values(:new.idProducto, 'ENTRADAS');
         elsif nom in ('Sopa de fideos','Sopa de hongos','Frijoles charros','Consome','Sopa azteca','Arroz a la mexicana','Arroz con mole o huevo','Pozole rojo o blanco') then
-                insert into categoria(idProducto,taqueGoria) values(:new.idProducto, 'Del Cazo');
+                insert into categoria(idProducto,taqueGoria) values(:new.idProducto, 'DEL CAZO');
         elsif nom in ('Sencillo de guisado','Pastor','Bistec, Chuleta, Chorizo o Pollo','Arrachera','Costilla') then
-                insert into categoria(idProducto,taqueGoria) values(:new.idProducto, 'Sopes y Huaraches');
+                insert into categoria(idProducto,taqueGoria) values(:new.idProducto, 'SOPES Y HUARACHES');
         elsif nom in ('Verdes, Rojas, Enfrijoladas o Con salsa guajillo con mole') then
-                insert into categoria(idProducto,taqueGoria) values(:new.idProducto, 'Enchiladas');
+                insert into categoria(idProducto,taqueGoria) values(:new.idProducto, 'ENCHILADAS');
         elsif nom in ('Fundido natural','Choriqueso','Chicharron de queso') then
-                insert into categoria(idProducto,taqueGoria) values(:new.idProducto, 'Quesos');
+                insert into categoria(idProducto,taqueGoria) values(:new.idProducto, 'QUESOS');
         elsif nom in ('Gringa','Gringa consentida','Gringa vaquera','Gringa loca','Quesadilla tradicional','Quesadilla','Quesadilla especial','Volcanes','Volcanes con carne') then
-                insert into categoria(idProducto,taqueGoria) values(:new.idProducto, 'Gringas, Quecas y Volcanes');        
+                insert into categoria(idProducto,taqueGoria) values(:new.idProducto, 'GRINGAS, QUECAS Y VOLCANES');        
         elsif nom in ('De pastor','Vegerariano','De bistec','De chuleta o pollo','De arrachera','De costilla') then
-                insert into categoria(idProducto,taqueGoria) values(:new.idProducto, 'Alambres');
+                insert into categoria(idProducto,taqueGoria) values(:new.idProducto, 'ALAMBRES');
         elsif nom in ('Ensalada','De la granja') then
-                insert into categoria(idProducto,taqueGoria) values(:new.idProducto, 'Ensaladas');
+                insert into categoria(idProducto,taqueGoria) values(:new.idProducto, 'ENSALADAS');
         elsif nom in ('El de pastor 2x1','Los de guisado','Bistec','Chuleta','Pechuga de pollo','Pastor de pollo','Chorizo','De Arrachera','De costilla') then
-                insert into categoria(idProducto,taqueGoria) values(:new.idProducto, 'Tacos');
+                insert into categoria(idProducto,taqueGoria) values(:new.idProducto, 'TACOS');
         elsif nom in ('100% Pastor','100% Carne de res','100% Pechuga de pollo','100% Arrachera') then
-                insert into categoria(idProducto,taqueGoria) values(:new.idProducto, 'Hamburguesas');
+                insert into categoria(idProducto,taqueGoria) values(:new.idProducto, 'HAMBURGUESAS');
         elsif nom in ('Torta de Pastor','Torta de Bistec','Torta de Milanesa','Torta de Arrachera','Torta de Pollo') then
-                insert into categoria(idProducto,taqueGoria) values(:new.idProducto, 'Tortas');
+                insert into categoria(idProducto,taqueGoria) values(:new.idProducto, 'TORTAS');
         elsif nom in ('Jarras de agua','Aguas frescas','Refrescos','Jugo de naranja','Naranjada','Cervezas','Micheladas','Cubanas','Cafe americano','Cafe de olla','Cafe capuchino','Chocolate con leche','Te','Cafe con leche','Vaso de leche') then
-                insert into categoria(idProducto,taqueGoria) values(:new.idProducto, 'Bebidas');
+                insert into categoria(idProducto,taqueGoria) values(:new.idProducto, 'BEBIDAS');
         elsif nom in ('Flan','Pastel') then
-                insert into categoria(idProducto,taqueGoria) values(:new.idProducto, 'Postres');
+                insert into categoria(idProducto,taqueGoria) values(:new.idProducto, 'POSTRES');
         else
-          insert into categoria(idProducto,taqueGoria) values(:new.idProducto, 'Salsas');
+          insert into categoria(idProducto,taqueGoria) values(:new.idProducto, 'SALSAS');
     end if;
 end;
 
@@ -41,6 +41,11 @@ CREATE TABLE Salsa_aux(
         idProducto INTEGER NOT NULL,
         presentacion VARCHAR2(70) NOT NULL,
         scoville VARCHAR2(20) NOT NULL
+);
+
+create table Recomendar_aux(
+    idProducto INTEGER NOT NULL,
+    idProductoSalsa INTEGER NOT NULL
 );
 
 create sequence salsa_seq start with 1;
@@ -51,6 +56,7 @@ for each row
 declare 
         salsa_count integer;
         salsaid integer;
+        productoid integer;
 begin  
         select salsa_seq.nextval
         into salsa_count
@@ -60,4 +66,10 @@ begin
         MINUS
         select idProducto from (select idProducto from (producto natural join categoria) where taqueGoria = 'Salsas' ) where rownum <salsa_count);
         insert into salsa_aux(idProducto, presentacion, scoville) values (salsaid, :new.presentacion, :new.scoville);
+        --aqui recomendamos cosas para las salsas
+        select idProducto into productoid from 
+        (select idProducto from (select idProducto from (producto natural join categoria) where taqueGoria <> 'Salsas' ) where rownum <salsa_count+1
+        MINUS
+        select idProducto from (select idProducto from (producto natural join categoria) where taqueGoria <> 'Salsas' ) where rownum <salsa_count);
+        insert into recomendar_aux(idProducto,idProductoSalsa) values (productoid, salsaid);
 end;
