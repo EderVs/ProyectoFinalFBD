64867d1578da9fc9646b65474638b0cba999f786
MiguelConcha <mconcha@ciencias.unam.mx>
2018-01-12 13:33:31 -0600

    Procedimientos almacenados con etiqueta de comentado.

--- Base de Datos/src/Procedimientos Almacenados/sp_elimina_caduco.sql
+++ Base de Datos/src/Procedimientos Almacenados/sp_elimina_caduco.sql
@@ -1,20 +1,37 @@
---para poder imprimir en pantalla
+-- ##########################################################################
+-- Nombre            : sp_elimina_caduco.sql.
+-- Fecha             : 12 de enero del 2018.
+-- Autores           : Flores Martínez Andrés, 
+--                     Vázquez Salcedo Eduardo Eder,
+--                     Sánchez Pérez Pedro Juan Salvador,
+--                     Concha Vázquez Miguel.
+-- Compañía          : Computólogos A.C., Facultad de Ciencias UNAM.
+-- Cliente           : Taquería Tacoste.
+-- ========================================================================
+-- Propósito         : Se crea un un procedimiento almacenado que será una función
+--                     cuyo proósito será el de eliminar aquellos ingredientes
+--                     que ya caducaron.
+-- ##########################################################################
+
+-- Para poder imprimir en pantalla.
 set serveroutput on format wrapped;
---procedimiento que elimina aquellos ingredientes que ya caducaron
+
 create or replace procedure elimina_caducos is
-	--creamos un cursor de los ingredientes caducados
+	-- Creamos un cursor de los ingredientes caducados.
 	cursor a_eliminar_cursor is
 		select idIngrediente, nombre
 		from ingrediente 
 		where fechaCaducidad < current_date;
 begin
-	--notificamos que ingredientes serán eliminados a la vez que eliminamos cada ingrediente
+	-- Notificamos que ingredientes serán eliminados a la vez que eliminamos cada ingrediente.
     dbms_output.put_line('Los siguientes ingredientes están caducados y serán eliminados');
 	for c in a_eliminar_cursor loop
 		dbms_output.put_line('idIngrediente: '||c.idIngrediente||', nombre: '||c.nombre);
         delete from ingrediente where idIngrediente = c.idIngrediente;
 	end loop;
 end elimina_caducos;
+
+-- Ejemplos: 
 /*
 INSERT INTO INGREDIENTE(IDINGREDIENTE, NOMBRE, MARCA, CANTIDADEXISTENCIA, FECHACADUCIDAD) VALUES
 (1, 'Cabtinfiator', 'Union E-Mobile Corporation', 4, TO_DATE('01/01/1970','dd/mm/yyyy'));
@@ -26,4 +43,5 @@ INSERT INTO INGREDIENTE(IDINGREDIENTE, NOMBRE, MARCA, CANTIDADEXISTENCIA, FECHAC
 (4, 'Monoceivor', 'South Space Research Inc.', 1, TO_DATE('03/09/1979','dd/mm/yyyy'));
 
 execute elimina_caducos;
-select * from ingrediente;*/
+select * from ingrediente;
+*/
--- Base de Datos/src/Procedimientos Almacenados/sp_incrementa_salario.sql
+++ Base de Datos/src/Procedimientos Almacenados/sp_incrementa_salario.sql
@@ -1,10 +1,26 @@
---función que incrementa el sueldo de los empleados en un porcentaje dado. 
---Será útil debido a la continua inflación monetaria
-create or replace procedure inc_sal(inc float) is
+-- ##########################################################################
+-- Nombre            : sp_incrementa_salario.sql.
+-- Fecha             : 12 de enero del 2018.
+-- Autores           : Flores Martínez Andrés, 
+--                     Vázquez Salcedo Eduardo Eder,
+--                     Sánchez Pérez Pedro Juan Salvador,
+--                     Concha Vázquez Miguel.
+-- Compañía          : Computólogos A.C., Facultad de Ciencias UNAM.
+-- Cliente           : Taquería Tacoste.
+-- ========================================================================
+-- Propósito         : Se crea un un procedimiento almacenado que será una función
+--                     cuyo proósito será el de incrementar el sueldo de los empleados
+--                     en un porcentaje dado. Será útil debido a la continua
+--                     inflación monetaria.
+-- ##########################################################################
+
+create or replace procedure inc_sal(inc float) is --Le pasamos el porcentaje de incremento.
 begin
 	update empleado
-	set salario = salario + salario*inc;
+	set salario = salario + salario*inc; --El salario actual del Empleado tiene que ser lo que era sumado en el salario por el incremento dado.
 end inc_sal;
+
+-- Ejemplos: 
 /*INSERT INTO SUCURSAL(IDSUCURSAL, HORAAPERTURA, HORACIERRE, MUNICIPIO, COLONIA, CALLE, CP, NUMINTERIOR, NUMEXTERIOR) VALUES
 (1, TO_DATE('02/12/1977','dd/mm/yyyy'), TO_DATE('03/12/1977','dd/mm/yyyy'), 'Elkland', 'Fakenham', '1954 Monument Road', 0, 95, 3914);
 INSERT INTO EMPLEADO(TAQUICLAVE, IDSUCURSAL, SALARIO, EMAIL, TELEFONO, NOMBRE, APELLIDOPATERNO, APELLIDOMATERNO, MUNICIPIO, COLONIA, CALLE, CP, NUMINTERIOR, NUMEXTERIOR, CURP, RFC, TIPO, TIPOSANGRE, NUMEMERGENCIA, FECHACONTRATACION) VALUES
@@ -15,4 +31,5 @@ INSERT INTO EMPLEADO(TAQUICLAVE, IDSUCURSAL, SALARIO, EMAIL, TELEFONO, NOMBRE, A
 (6232990399, 1, 2.456, 'MerrieI.Seals@example.com', '(454) 654-8717', 'Wendell', 'Gandy', 'Smalls', 'Howard Lake', 'Dromore (Co Down)', '89th FL', 2, 97, 1940, 2, 'BH 12 01 20 A', 'TAQUERO', 'O+', '(525) 709-9587', TO_DATE('02/12/1977','dd/mm/yyyy'));
 select * from empleado;
 execute inc_sal(0.15);
-select * from empleado;/*
+select * from empleado;
+/*
